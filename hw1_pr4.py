88cc149596422eb331b48a9727b687353d7f6012
Martha Garcia <mgcavero04@hotmail.com>
2024-09-28 20:17:03 -0400

    Update hw1_pr4.py

--- hw1_pr4.py
+++ hw1_pr4.py
@@ -1,3 +1,18 @@
+''''For each 0 <= i < len(nums1), find the index j such that nums1[i] ==
+nums2[j] and determine the next greater element of nums2[j] in nums2.
+If there is no next greater element, then the answer for this query is -1.
+Return an array ans of length len(nums1) such that ans[i] is the next
+greater element as described above.
+The run-time must be in O(n + m), where n and m are sizes of the
+arrays.
+Example 1.
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is
+as follows:
+• for 4. There is no next greater element, so the answer is -1.
+• for 1. The next greater element is 3.
+• for 2. There is no next greater element, so the answer is -1.'''
 class Stack:
     def greater(nums1 , nums2): #For each 0 <= n < len(nums1), find the index m such that nums1[n] ==nums2[m] and determine the next greater element of nums2[m] in nums2. If there is no next greater element, then the answer for this query is -1. Return an array ans of length len(nums1) such that ans[n] is the next greater element as described above.
         stack=[]
